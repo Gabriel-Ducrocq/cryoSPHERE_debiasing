c1b584546acda4823a28ecd77987a3195c344255
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-09-26 12:03:09 +0200

    fixing the spacing in BaseGrid

--- cryosphere/model/gmm.py
+++ cryosphere/model/gmm.py
@@ -75,38 +75,38 @@ class Gaussian_splatting(torch.nn.Module):
 
 
 class BaseGrid(torch.nn.Module):
-	"""
-	Grid spanning origin, to origin + (side_shape - 1) * voxel_size, for the coordinate of each pixel.
-	"""
-	def __init__(self, side_n_pixels, voxel_size, origin=None, device="cpu"):
-		"""
-		:param side_n_pixels: integer, number of pixel on each side of the image.
-		:param voxel_size: float, size of each pixel in Å.
-		:param origin: float, origin on the image.
-		:param device: torch device on which to perform the computations.
-    	"""
-		super().__init__()
-		self.side_n_pixels = side_n_pixels
-		self.voxel_size = voxel_size
-		if not origin:
-			origin = 0
+    """
+    Grid spanning origin, to origin + (side_shape - 1) * voxel_size, for the coordinate of each pixel.
+    """
+    def __init__(self, side_n_pixels, voxel_size, origin=None, device="cpu"):
+        """
+        :param side_n_pixels: integer, number of pixel on each side of the image.
+        :param voxel_size: float, size of each pixel in Å.
+        :param origin: float, origin on the image.
+        :param device: torch device on which to perform the computations.
+        """
+        super().__init__()
+        self.side_n_pixels = side_n_pixels
+        self.voxel_size = voxel_size
+        if not origin:
+            origin = 0
 
 
-		self.origin = origin
+        self.origin = origin
 
-		line_coords = torch.linspace(origin, (side_n_pixels - 1) * voxel_size + origin, side_n_pixels, device=device)
-		self.register_buffer("line_coords", line_coords)
-		[xx, yy] = torch.meshgrid([self.line_coords, self.line_coords], indexing="ij")
-        image_format_plan_coord = torch.stack([xx, yy], dim=-1)
-		plane_coords = image_format_plan_coord.reshape(-1, 2)
-		self.register_buffer("plane_coords", plane_coords)
+        line_coords = torch.linspace(origin, (side_n_pixels - 1) * voxel_size + origin, side_n_pixels, device=device)
+        self.register_buffer("line_coords", line_coords)
+        [xx, yy] = torch.meshgrid([self.line_coords, self.line_coords], indexing="ij")
+        image_format_plane_coords = torch.stack([xx, yy], dim=-1)
+        plane_coords = image_format_plane_coords.reshape(-1, 2)
+        self.register_buffer("plane_coords", plane_coords)
         self.register_buffer("image_format_plane_coords", image_format_plane_coords)
-		self.plane_shape = (self.side_n_pixels, self.side_n_pixels)
+        self.plane_shape = (self.side_n_pixels, self.side_n_pixels)
 
-		[xx, yy, zz] = torch.meshgrid([self.line_coords, self.line_coords, self.line_coords])
-		vol_coords = torch.stack([xx, yy, zz], dim=-1).reshape(-1, 3)
-		self.register_buffer("vol_coords", vol_coords)
-		self.vol_shape = (self.side_n_pixels, self.side_n_pixels, self.side_n_pixels)
+        [xx, yy, zz] = torch.meshgrid([self.line_coords, self.line_coords, self.line_coords])
+        vol_coords = torch.stack([xx, yy, zz], dim=-1).reshape(-1, 3)
+        self.register_buffer("vol_coords", vol_coords)
+        self.vol_shape = (self.side_n_pixels, self.side_n_pixels, self.side_n_pixels)
 
 
 
